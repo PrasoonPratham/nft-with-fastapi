7c1218a85f47c9afedbb7585de2087c4c3e50153
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-12-06 15:09:15 -0800

    Completed project

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import { ThirdwebProvider } from "@3rdweb/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import "tailwindcss/tailwind.css";
 
-const supportedChainIds = [1,4,137];
+const supportedChainIds = [1, 4, 137];
 const connectors = {
   injected: {},
 };
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-     <ThirdwebProvider
-      connectors={connectors}
-      supportedChainIds={supportedChainIds}
-    >
-      <Component {...pageProps} />
-    </ThirdwebProvider>
+    <ChakraProvider>
+      <ThirdwebProvider
+        connectors={connectors}
+        supportedChainIds={supportedChainIds}
+      >
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </ChakraProvider>
   );
 };
 
